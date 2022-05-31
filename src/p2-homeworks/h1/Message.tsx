616fde7e06723f71b333a965784bf1d2fb2119a1
roodi <igorrud95@gmail.com>
2022-05-31 23:01:46 +0700

    'fix'

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -5,9 +5,8 @@ import style from './Message.module.css'
 function Message(props: MessageType) {
     return (
         <div className={style.message}>
-            <div className={style.message_image}>
-                <img src={props.avatar} alt="avatar"/>
-            </div>
+            <img src={props.avatar} alt="avatar" className={style.avatar}/>
+            <div className={style.circle}/>
             <div className={style.message_box}>
                 <div className={style.name}>{props.name}</div>
                 <div className={style.messages}>{props.message}</div>
