7e937d7005c60d38f3c310eb91171eda9ab65bd3
roodi <igorrud95@gmail.com>
2022-08-31 23:56:37 +0700

    HW6

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -5,6 +5,7 @@ import s from './App.module.css'
 // import HW3 from '../../../p2-homeworks/h3/HW3';
 // import HW4 from '../../../p2-homeworks/h4/HW4';
 import HW5 from '../../../p2-homeworks/h5-rrd-v6/HW5';
+import HW6 from '../../../p2-homeworks/h6/HW6';
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
             {/*<HW3/>*/}
             {/*<HW4/>*/}
             <HW5/>
+            <HW6 />
 
         </div>
     )
